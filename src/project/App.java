8c4c29fcd7454f01c4c9f6bb2f652a35626942b0
Yanis <yanisrichard@live.fr>
2019-10-25 15:19:37 +0200

    init

---
+++ src/project/App.java
@@ -0,0 +1,30 @@
+package project;
+
+import project.views.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+
+    private static JFrame frame = new JFrame("Processus Stochastique");
+
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        frame.setContentPane(new Queue().mainJPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        refreshJFrame();
+    }
+
+    public static void refreshJFrame() {
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
